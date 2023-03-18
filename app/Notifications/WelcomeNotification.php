56e8a3536baf36f3db468b6189fd3ac1e8c70fdc
Jul <jul@fangdom.de>
2023-03-18 23:02:52 +0100

    Changed email image and salutation

--- app/Notifications/WelcomeNotification.php
+++ app/Notifications/WelcomeNotification.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Support\HtmlString;
 
 class WelcomeNotification extends Notification implements ShouldQueue
 {
@@ -27,7 +28,8 @@ class WelcomeNotification extends Notification implements ShouldQueue
             ->subject('Application Phase: Thank you!')
             ->line('Thank you for your application for a Dealership at the upcoming Eurofurence. Your interest in being a part of this year\'s Dealers\' Den is very much appreciated.')
             ->line('We have received your application and will review it once the Dealership application period has ended. We understand that waiting can be difficult, but please know that we are working hard to review all applications in a timely manner. Once we have reviewed all the applications, we will get in touch with you to provide you with all the necessary information about the next steps.')
-            ->line('Thank you in advance for your patience. The Dealers\' Den management is looking forward to reviewing your application.');            
+            ->line('Thank you in advance for your patience. The Dealers\' Den management is looking forward to reviewing your application.')            
+            ->salutation(new HtmlString('Best regards,<br />Pattarchus'));
     }
 
     public function toArray($notifiable): array
