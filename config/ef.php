3559be831bfb6f95bd263d1688a2a180844f8dfa
JulSteele <20680187+julsteele@users.noreply.github.com>
2023-03-22 22:12:26 +0100

    Made URLs visible in emails

--- config/ef.php
+++ config/ef.php
@@ -4,4 +4,6 @@ use Carbon\Carbon;
 return [
     'reg_end_date' => Carbon::create(2023, 4, 30, 23, 59, 0, 'Europe/Berlin'),
     'dealers_tos_url' => 'https://www.eurofurence.org/EF27/dealersden/',
+    'idp_url' => 'https://identity.eurofurence.org',
+    'dealers_email' => 'dealers@eurofurence.org',
 ];
