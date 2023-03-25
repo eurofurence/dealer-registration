a81d1efc3b8c3e7c77fa14f84cd08281a2216f05
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2023-03-25 02:57:21 +0100

    perform logout in session and OIDC

--- app/Http/Controllers/Auth/FrontChannelLogoutController.php
+++ app/Http/Controllers/Auth/FrontChannelLogoutController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class FrontChannelLogoutController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
         \Auth::logout();
+
+        $idToken = Session::get('access_token')?->getValues()['id_token'];
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect("https://identity.eurofurence.org/oauth2/sessions/logout?id_token_hint={$idToken}");
     }
 }
