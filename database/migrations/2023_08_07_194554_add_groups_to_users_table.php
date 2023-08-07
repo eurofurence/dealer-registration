46c37932b1f7cd6c749eebe8a273ba6f58fc42df
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2023-08-07 22:34:33 +0200

    fix default value for users.groups in migration

--- database/migrations/2023_08_07_194554_add_groups_to_users_table.php
+++ database/migrations/2023_08_07_194554_add_groups_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->json('groups')->default(null);
+            $table->json('groups')->default('[]');
         });
     }
 
