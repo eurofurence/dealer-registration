b5544b1f1579dd4e07691c921714c15e810677e8
JulSteele <20680187+julsteele@users.noreply.github.com>
2023-08-10 23:25:47 +0200

    Added dates, formatted comment view

--- app/Filament/Resources/CommentResource.php
+++ app/Filament/Resources/CommentResource.php
@@ -21,15 +21,24 @@ class CommentResource extends Resource
     public static function form(Form $form): Form
     {
         return $form
+            ->columns(3)
             ->schema([
-                Forms\Components\Textarea::make('text')
-                    ->required()
-                    ->maxLength(2048),
-                Forms\Components\Toggle::make('admin_only'),
-                Forms\Components\Select::make('user_id')->searchable()->relationship('author', 'name')
-                    ->required(),
-                Forms\Components\Select::make('application_id')->searchable()->relationship('application', 'id')
-                    ->required(),
+                Forms\Components\Group::make()->columnSpan(2)->columns()->schema([
+                    Forms\Components\Group::make()->columns(1)->schema([
+                        Forms\Components\Select::make('user_id')->searchable()->relationship('author', 'name')
+                            ->required(),
+                        Forms\Components\Select::make('application_id')->searchable()->relationship('application', 'id')
+                            ->required(),
+                        Forms\Components\Toggle::make('admin_only'),
+                    ]),
+                    Forms\Components\Textarea::make('text')
+                            ->required()
+                            ->maxLength(2048),
+                ]),
+                Forms\Components\Fieldset::make('Dates')->columnSpan(1)->columns()->schema([
+                    Forms\Components\Placeholder::make('created_at')->content(fn (?Comment $record): string => $record?->created_at?->diffForHumans() ?? '-'),
+                    Forms\Components\Placeholder::make('updated_at')->content(fn (?Comment $record): string => $record?->updated_at?->diffForHumans() ?? '-'),
+                ]),
 
             ]);
     }
