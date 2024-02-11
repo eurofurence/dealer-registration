2522bbf48344764d40c6325b37e1db782a301619
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2024-02-11 23:39:18 +0100

    feat(regsys): implement addinfo API

--- app/Http/Controllers/Client/RegSysClientController.php
+++ app/Http/Controllers/Client/RegSysClientController.php
@@ -4,34 +4,37 @@ namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
 use App\Models\TableType;
-
+use Exception;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\Container\ContainerExceptionInterface;
 
 class RegSysClientController extends Controller
 {
-    public static function getPackages(string|null $reg_id): mixed
+    public static function getPackages(string|null $regId): mixed
     {
-        if (!self::validateParam($reg_id, 'reg id')) {
+        if (!self::validateParam($regId, 'reg id')) {
             return [];
         }
 
         $response = Http::get(config('services.regsys.url') . '/package-api', [
             'token' => config('services.regsys.token'),
-            'id' => $reg_id,
+            'id' => $regId,
         ]);
 
         if ($response->ok()) {
             return $response->json('packages');
         } else {
-            self::logError("Registration with id " . $reg_id . " could not be retrieved, reason: " . $response->reason());
+            self::logError("Registration with id " . $regId . " could not be retrieved, reason: " . $response->reason());
             return [];
         }
     }
 
-    public static function bookPackage(string|null $reg_id, TableType $tableType): bool
+    public static function bookPackage(string|null $regId, TableType $tableType): bool
     {
-        if (!self::validateParam($reg_id, 'reg id')) {
+        if (!self::validateParam($regId, 'reg id')) {
             return false;
         }
         if (!self::validateParam($tableType, 'table type')) {
@@ -40,7 +43,7 @@ class RegSysClientController extends Controller
 
         $url = config('services.regsys.url')
             . '/package-api?token=' . config('services.regsys.token')
-            . "&id=" . $reg_id
+            . "&id=" . $regId
             . "&package=" . $tableType->package;
 
         $response = Http::post($url);
@@ -48,14 +51,14 @@ class RegSysClientController extends Controller
         if ($response->ok()) {
             return true;
         } else {
-            self::logError("Package for id " . $reg_id . " could not be booked, reason: " . $response->reason());
+            self::logError("Package for id " . $regId . " could not be booked, reason: " . $response->reason());
             return false;
         }
     }
 
-    public static function removePackage(string|null $reg_id, TableType $tableType): bool
+    public static function removePackage(string|null $regId, TableType $tableType): bool
     {
-        if (!self::validateParam($reg_id, 'reg id')) {
+        if (!self::validateParam($regId, 'reg id')) {
             return false;
         }
         if (!self::validateParam($tableType, 'table type')) {
@@ -64,15 +67,15 @@ class RegSysClientController extends Controller
 
         $url = config('services.regsys.url')
             . '/package-api?token=' . config('services.regsys.token')
-            . "&id=" . $reg_id
+            . "&id=" . $regId
             . "&package=" . $tableType->package;
 
-        $response = Http::delete( $url);
+        $response = Http::delete($url);
 
         if ($response->ok()) {
             return true;
         } else {
-            self::logError("Package for id " . $reg_id . " could not be deleted, reason: " . $response->reason());
+            self::logError("Package for id " . $regId . " could not be deleted, reason: " . $response->reason());
             return false;
         }
     }
@@ -91,15 +94,15 @@ class RegSysClientController extends Controller
         }
     }
 
-    public static function getSingleReg(string|null $reg_id): mixed
+    public static function getSingleReg(string|null $regId): mixed
     {
-        if (!self::validateParam($reg_id, 'reg id')) {
+        if (!self::validateParam($regId, 'reg id')) {
             return null;
         }
 
         $response = Http::get(config('services.regsys.url') . '/dealers-den-api', [
             'token' => config('services.regsys.token'),
-            'id' => $reg_id,
+            'id' => $regId,
         ]);
 
         if ($response->ok()) {
@@ -107,13 +110,84 @@ class RegSysClientController extends Controller
                 return $response->json('result')[0];
             } else {
                 // If reg ID is invalid, regsys returns HTTP 200 but an empty array.
-                self::logError("Registration with id " . $reg_id . " could not be retrieved, reg id invalid.");
+                self::logError("Registration with id " . $regId . " could not be retrieved, reg id invalid.");
                 return null;
             }
         } else {
-            self::logError("Registration with id " . $reg_id . " could not be retrieved, reason: " . $response->reason());
+            self::logError("Registration with id " . $regId . " could not be retrieved, reason: " . $response->reason());
             return null;
         }
+    }
+
+    /**
+     * Set the `dealerreg` additional info flag for the given registration.
+     *
+     * @param string $regId Registration number to set the flag for
+     * @param bool $hasDealerReg true if the flag should be set
+     * @return bool|null true or false depending on whether the flag was successfully set to the requested on the registration, null if an error was encountered.
+     * @throws BindingResolutionException
+     * @throws NotFoundExceptionInterface
+     * @throws ContainerExceptionInterface
+     * @throws Exception
+     */
+    public static function setAdditionalInfoDealerReg(string $regId, bool $hasDealerReg): bool|null
+    {
+        if (!self::validateParam($regId, 'reg id')) {
+            return null;
+        }
+
+        $httpRequest = Http::withQueryParameters([
+            'token' => config('services.regsys.token'),
+            'area' => 'dealerreg',
+            'id' => $regId,
+        ]);
+
+        $response = null;
+        if ($hasDealerReg) {
+            $response = $httpRequest->post(config('services.regsys.url') . '/addinfo-api');
+        } else {
+            $response = $httpRequest->delete(config('services.regsys.url') . '/addinfo-api');
+        }
+
+        $result = $response?->json();
+
+        if (!$response?->ok() || $result['ok'] !== true) {
+            self::logError("Additional info 'dealerreg' for registration with id {$regId} could not be set to {$hasDealerReg}, reason: " . ($response?->reason() ?? 'request failed') . " " . $result['message'] ?? 'for unknown reason');
+            return null;
+        }
+
+        return $result['enabled'] === $hasDealerReg;
+    }
+
+    /**
+     * Check whether the `dealerreg` additional info flag is set for the given registration.
+     *
+     * @param string $regId Registration number to retrieve the flag for
+     * @return bool|null true or false depending on whether the flag is set on the registration, null if an error was encountered.
+     * @throws BindingResolutionException
+     * @throws NotFoundExceptionInterface
+     * @throws ContainerExceptionInterface
+     */
+    public static function getAdditionalInfoDealerReg(string $regId): bool|null
+    {
+        if (!self::validateParam($regId, 'reg id')) {
+            return null;
+        }
+
+        $response = Http::get(config('services.regsys.url') . '/addinfo-api', [
+            'token' => config('services.regsys.token'),
+            'area' => 'dealerreg',
+            'id' => $regId,
+        ]);
+
+        $result = $response?->json();
+
+        if (!$response?->ok() || $result['ok'] !== true) {
+            self::logError("Additional info 'dealerreg' for registration with id " . $regId . " could not be retrieved, reason: " . ($response?->reason() ?? 'request failed') . " " . $result['message'] ?? 'for unknown reason');
+            return null;
+        }
+
+        return $result['enabled'];
     }
 
     private static function validateParam(object|string|null $param, string $type): bool
