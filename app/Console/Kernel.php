190648dd99b1f05fc584ce828a55afd5a791f9ad
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2024-02-14 00:48:02 +0100

    feat(regsys): make regsys sync schedule configurable

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->job(new SynchronizeRegsys)->hourlyAt(23);
+        if (config('con.cron_regsys_sync')) {
+            $schedule->job(new SynchronizeRegsys)->cron(config('con.cron_regsys_sync'));
+        }
     }
 
     /**
