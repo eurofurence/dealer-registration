6938ae5d697ebf680550739b1a3b649a65a7114d
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2024-02-16 16:25:18 +0100

    fix(ui): access user directly instead of via potentially missing application

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,19 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Client\RegSysClientController;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function __invoke()
     {
+        /** @var User */
         $user = Auth::user();
         $registrationId = $user->reg_id;
 
         $application = $user->application;
 
         if (!$registrationId && $registrationId = RegSysClientController::getRegistrationIdForCurrentUser()) {
-            $application->user()->update(['reg_id' => $registrationId]);
+            $user->update(['reg_id' => $registrationId]);
         }
 
         $registration = $registrationId ? RegSysClientController::getSingleReg($registrationId) : null;
