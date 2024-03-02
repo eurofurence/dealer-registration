75c381adb1f2eae0c8746fa7abe0faf449aa5bdd
Fenrikur <3359222+Fenrikur@users.noreply.github.com>
2024-03-02 13:36:57 +0100

    fix(admin): catch some potential exceptions during initialisation

--- app/Filament/Widgets/ApplicationStats.php
+++ app/Filament/Widgets/ApplicationStats.php
@@ -12,12 +12,16 @@ class ApplicationStats extends BaseWidget
 {
     protected function getStats(): array
     {
-        $data = Cache::remember('dd-admin-application-stats', 60, fn() => Application::query()->toBase()->select(DB::raw('type, COUNT(*) as count'))->whereNull('canceled_at')->whereNull('waiting_at')->groupBy('type')->get());
-        return [
-            Stat::make('Total Applications (active)', fn() => $data->sum('count')),
-            Stat::make('Total Dealers (active)', fn() => $data->firstWhere('type', 'dealer')?->count ?? 0),
-            Stat::make('Total Shares (active)', fn() => $data->firstWhere('type', 'share')?->count ?? 0),
-            Stat::make('Total Assistants (active)', fn() => $data->firstWhere('type', 'assistant')?->count ?? 0),
-        ];
+        try {
+            $data = Cache::remember('dd-admin-application-stats', 60, fn() => Application::query()->toBase()->select(DB::raw('type, COUNT(*) as count'))->whereNull('canceled_at')->whereNull('waiting_at')->groupBy('type')->get());
+            return [
+                Stat::make('Total Applications (active)', fn() => $data->sum('count')),
+                Stat::make('Total Dealers (active)', fn() => $data->firstWhere('type', 'dealer')?->count ?? 0),
+                Stat::make('Total Shares (active)', fn() => $data->firstWhere('type', 'share')?->count ?? 0),
+                Stat::make('Total Assistants (active)', fn() => $data->firstWhere('type', 'assistant')?->count ?? 0),
+            ];
+        } catch (\Throwable $th) {
+            return [];
+        }
     }
 }
--- app/Filament/Widgets/ApplicationTablesAssignedChart.php
+++ app/Filament/Widgets/ApplicationTablesAssignedChart.php
@@ -13,11 +13,15 @@ class ApplicationTablesAssignedChart extends AbstractApplicationTablesChart
 
     protected function retrieveData(): array
     {
-        return Cache::remember('dd-admin-application-tables-assigned', 60, function(): array {
-            $tableTypeCount = Application::query()->toBase()->join('table_types', 'table_type_assigned', '=', 'table_types.id')->select(DB::raw('table_types.id as id, COUNT(*) as count'))->where('type', '=', 'dealer')->whereNull('canceled_at')->whereNull('waiting_at')->groupBy('table_types.id')->get()->pluck('count', 'id');
-            return TableType::all()->mapWithKeys(function(TableType $tableType) use ($tableTypeCount) {
-                return [ $tableType->name => $tableTypeCount[$tableType->id] ?? 0];
-            })->toArray();
-        });
+        try {
+            return Cache::remember('dd-admin-application-tables-assigned', 60, function(): array {
+                $tableTypeCount = Application::query()->toBase()->join('table_types', 'table_type_assigned', '=', 'table_types.id')->select(DB::raw('table_types.id as id, COUNT(*) as count'))->where('type', '=', 'dealer')->whereNull('canceled_at')->whereNull('waiting_at')->groupBy('table_types.id')->get()->pluck('count', 'id');
+                return TableType::all()->mapWithKeys(function(TableType $tableType) use ($tableTypeCount) {
+                    return [ $tableType->name => $tableTypeCount[$tableType->id] ?? 0];
+                })->toArray();
+            });
+        } catch (\Throwable $th) {
+            return [];
+        }
     }
 }
