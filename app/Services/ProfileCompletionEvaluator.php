fea2e6d7ee893feaf20fa21cb77f0312dc7579a2
SharkyTheWhite <git@sharkythewhite.eu>
2025-08-18 22:42:07 +0200

    fix(profile): Handle null profile case in completion progress.

--- app/Services/ProfileCompletionEvaluator.php
+++ app/Services/ProfileCompletionEvaluator.php
@@ -51,7 +51,7 @@ class ProfileCompletionEvaluator
         $profile = $application->profile;
 
         // Allow shares to share profile, in that case we display them a completed form (no leak information from main).
-        if ($isShare && $profile->is_hidden) {
+        if ($isShare && $profile?->is_hidden) {
             $this->addStep(new ProfileCompletionStep(
                 'hidden_share', 'Share Profile',
                 description: "You chose to hide your own profile, so you do not need to set anything. Please still make sure your main dealership profile is well filled!",
@@ -66,7 +66,7 @@ class ProfileCompletionEvaluator
             $this->addStep(new ProfileCompletionStep(
                 'image_thumbnail', 'Thumbnail Image',
                 description: "Please provide at least this image: A picture to go along with your dealership name in lists and overviews.",
-                score: self::string_has_chars($profile->image_thumbnail) > 3,
+                score: self::string_has_chars($profile?->image_thumbnail) > 3,
                 weight: 5, // This is VERY important! It is everywhere
             ));
 
@@ -75,7 +75,7 @@ class ProfileCompletionEvaluator
             $this->addStep(new ProfileCompletionStep(
                 'short_desc', 'Short Description',
                 description: "Give your dealership an inviting short description of at least $expectCharsAtLeast characters.",
-                score: self::string_has_chars($profile->short_desc),
+                score: self::string_has_chars($profile?->short_desc),
                 maxScore: $expectCharsAtLeast,
                 weight: 4, // This is important! It is in the profile header
             ));
@@ -84,13 +84,13 @@ class ProfileCompletionEvaluator
             $this->addStep(new ProfileCompletionStep(
                 'image_artist', 'Profile Image',
                 description: "A profile picture to go along with your dealership description.",
-                score: self::string_has_chars($profile->image_artist) > 3,
+                score: self::string_has_chars($profile?->image_artist) > 3,
                 weight: 3, // This is VERY important! It is everywhere!
             ));
 
             // "Keywords"
             $expectCharsAtLeast = 1;
-            $keywordCount = count($profile->keywords);
+            $keywordCount = count($profile?->keywords);
             $this->addStep(new ProfileCompletionStep(
                 'keywords', 'Set Keywords',
                 description: "You should tick all keywords that match what you offer, so people find you.",
@@ -104,14 +104,14 @@ class ProfileCompletionEvaluator
             $this->addStep(new ProfileCompletionStep(
                 'artist_desc', 'About You',
                 description: "Describe yourself in the \"About the Artist\" field with at least $expectCharsAtLeast characters.",
-                score: self::string_has_chars($profile->artist_desc),
+                score: self::string_has_chars($profile?->artist_desc),
                 maxScore: $expectCharsAtLeast,
                 weight: 2, // Also quite important
             ));
 
             // "Showcase Image"
-            $hasShowcaseImage = self::string_has_chars($profile->image_art) > 0;
-            $hasShowcaseText = self::string_has_chars($profile->art_preview_caption) > 5;
+            $hasShowcaseImage = self::string_has_chars($profile?->image_art) > 0;
+            $hasShowcaseText = self::string_has_chars($profile?->art_preview_caption) > 5;
             $this->addStep(new ProfileCompletionStep(
                 'showcase_image', 'Showcase Image',
                 description: "Provide a Showcase Image with a Caption to present what you are offering.",
@@ -124,7 +124,7 @@ class ProfileCompletionEvaluator
             $this->addStep(new ProfileCompletionStep(
                 'art_desc', 'About your Art',
                 description: "Describe in more detail what you are offering in the \"About the Art\" field with at least $expectCharsAtLeast characters.",
-                score: self::string_has_chars($profile->art_desc),
+                score: self::string_has_chars($profile?->art_desc),
                 maxScore: $expectCharsAtLeast,
                 weight: 1,
             ));
@@ -132,17 +132,17 @@ class ProfileCompletionEvaluator
             $this->addStep(new ProfileCompletionStep(
                 'website', 'Link your Website',
                 description: "If you have a website, please add it!",
-                score: self::string_has_chars($profile->website) > 3,
+                score: self::string_has_chars($profile?->website) > 3,
                 weight: 3,
             ));
 
             $expectCharsAtLeast = 2;
             $linkCount = 0;
-            if (self::string_has_chars($profile->twitter) > 3) $linkCount++;
-            if (self::string_has_chars($profile->mastodon) > 3) $linkCount++;
-            if (self::string_has_chars($profile->bluesky) > 3) $linkCount++;
-            if (self::string_has_chars($profile->telegram) > 3) $linkCount++;
-            if (self::string_has_chars($profile->discord) > 3) $linkCount++;
+            if (self::string_has_chars($profile?->twitter) > 3) $linkCount++;
+            if (self::string_has_chars($profile?->mastodon) > 3) $linkCount++;
+            if (self::string_has_chars($profile?->bluesky) > 3) $linkCount++;
+            if (self::string_has_chars($profile?->telegram) > 3) $linkCount++;
+            if (self::string_has_chars($profile?->discord) > 3) $linkCount++;
             $this->addStep(new ProfileCompletionStep(
                 'social_links', 'Link your Socials',
                 description: "Give at least $expectCharsAtLeast ways to find you via social media links (other than your website).",
@@ -155,7 +155,7 @@ class ProfileCompletionEvaluator
             $this->addStep(new ProfileCompletionStep(
                 'tweet', 'Advertisement Text',
                 description: "Write a short advertisement message of at least $expectCharsAtLeast characters.",
-                score: self::string_has_chars($profile->tweet),
+                score: self::string_has_chars($profile?->tweet),
                 maxScore: $expectCharsAtLeast,
                 weight: 1,
             ));
