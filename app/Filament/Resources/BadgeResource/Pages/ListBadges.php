9dab6f4cfdf11a16453f1c418c74280ca0a38c73
Diftraku <Diftraku@users.noreply.github.com>
2025-08-21 00:52:14 +0300

    feat(admin): add settings for handling font and background uploads

--- app/Filament/Resources/BadgeResource/Pages/ListBadges.php
+++ app/Filament/Resources/BadgeResource/Pages/ListBadges.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\BadgeResource\Pages;
 use App\Filament\Resources\BadgeResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Facades\Storage;
 
 class ListBadges extends ListRecords
 {
@@ -12,8 +13,29 @@ class ListBadges extends ListRecords
 
     protected function getHeaderActions(): array
     {
-        return [
-            Actions\CreateAction::make(),
-        ];
+        $actions = [];
+
+        $settingsAction = Actions\Action::make("settings")
+                    ->label(__("Settings"))
+                    ->icon('heroicon-o-cog-6-tooth')
+                    ->url(fn () => BadgeResource::getUrl('settings'));
+
+        
+        // Check if everything looks good
+        $badge_font = Storage::disk('local')->exists('badges/badge-font');
+        $badge_background = Storage::disk('local')->exists('badges/badge-background'); 
+        if (!$badge_font || !$badge_background) {
+            $settingsAction = $settingsAction
+                                ->color("danger")
+                                ->icon('heroicon-o-exclamation-triangle');
+        }
+
+        // Yeet
+        $actions[] = $settingsAction;
+
+        $actions[] = Actions\CreateAction::make()
+                    ->icon('heroicon-o-paint-brush')
+                    ->label(__("Custom Badge"));
+        return $actions;
     }
 }
