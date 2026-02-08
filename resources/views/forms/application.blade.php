773decedcdfbfe6a209f16464c2b9cb5b372f9d2
Diftraku <Diftraku@users.noreply.github.com>
2026-02-08 18:21:58 +0200

    fix(application): add form-check to the div for resale checkbox

--- resources/views/forms/application.blade.php
+++ resources/views/forms/application.blade.php
@@ -183,7 +183,7 @@
                                 No items may be offered that infringe upon the rights of third parties, including unlicensed third-party intellectual property.
                             </div>
                         </div>
-                        <div>
+                        <div class="form-check">
                         <input type="hidden" name="no_resale" value="0">
                         <input class="form-check-input @error('no_resale') is-invalid @enderror" name="no_resale"
                         @checked(old('_token') ? old('no_resale') : (Route::is('applications.edit') ? empty($application?->resale_description) : false)) @disabled(Carbon\Carbon::parse(config('convention.reg_end_date'))->isPast()) type="checkbox"
